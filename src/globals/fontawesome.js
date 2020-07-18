c34307259a13038b161d8253054876cc5faf7006
a-helen1 <andrewhelen1@gmail.com>
2020-07-18 16:22:01 +0100

    initial commit

---
+++ src/globals/fontawesome.js
@@ -0,0 +1,11 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+    faStar, 
+    faFilm, 
+    faVideo,
+    faHome,
+    faCalendar,
+    faArrowCircleLeft,
+}   from "@fortawesome/free-solid-svg-icons";
+
+library.add(faFilm, faStar, faVideo, faHome, faCalendar, faArrowCircleLeft);
