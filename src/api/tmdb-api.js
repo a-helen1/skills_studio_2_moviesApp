4a52c21152e794c83f51bbde6de2f8a35130fa4a
a-helen1 <andrewhelen1@gmail.com>
2020-08-18 19:55:01 +0100

    add usePerson hook and person api request

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -36,3 +36,10 @@ export const getMovies = () => {
     )
       .then(res => res.json());
   };
+
+  export const getPerson = id => {
+    return fetch(
+      `https://api.themoviedb.org/3/person/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
+    )
+      .then(res => res.json());
+  };
